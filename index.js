e425688e9a7054c320f7791c9bdb9e160b867cfa
Erick Villeda <eeverickeev@gmail.com>
2020-07-23 00:48:46 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,12 @@
+function sayHelloTo(firstName) {
+  console.log(`Hello, ${firstName}`)
+}
+sayHelloTo('Isabel')
+sayHelloTo('Jane')
+sayHelloTo('R2-D2')
+sayHelloTo(1)
+function say(greeting,firstname) {
+  console.log(`${greeting}, ${firstname}!`)
+}
+say("Goodbye","Julio")
+say("Julio", "hello")
